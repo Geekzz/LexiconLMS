4bfebf3e7ad00ce81fc61b74d325e065a1fef063
dansaa8 <dansaa8@protonmail.com>
2025-01-09 10:04:09 +0100

    Create Entites for database

---
+++ Domain.Models/Entities/Activity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Entities
+{
+    public class Activity
+    {
+        public int ActivityId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int ActivityTypeId { get; set; }
+        public ActivityType ActivityType { get; set; }
+
+        public int ModuleId { get; set; }
+        public Module Module { get; set; }
+
+        public int DocumentId { get; set; }
+        public Document Document { get; set; }
+    }
+}
--- Domain.Models/Entities/ApplicationUser.cs
+++ Domain.Models/Entities/ApplicationUser.cs
@@ -8,4 +8,12 @@ public class ApplicationUser : IdentityUser
 {
     public string? RefreshToken { get; set; }
     public DateTime RefreshTokenExpireTime { get; set; }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email {  get; set; }
+
+    public int CourseId { get; set; }
+    public Course Course { get; set; }
+
 }
