033e97fe3032d068b20cba1741475523f253368b
dansaa8 <dansaa8@protonmail.com>
2025-01-10 10:02:46 +0100

    Add Module controller + endpoint to get one module.

--- LMS.Presemtation/Controllers/ModuleController.cs
+++ LMS.Presemtation/Controllers/ModuleController.cs
@@ -1,30 +1,30 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using LMS.Shared.DTOs;
-//using Microsoft.AspNetCore.Mvc;
-//using Services.Contracts;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LMS.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
 
-//namespace LMS.Presentation.Controllers
-//{
-//    [Route("api/modules")]
-//    [ApiController]
-//    public class ModuleController : ControllerBase
-//    {
-//        private readonly IServiceManager _serviceManager;
+namespace LMS.Presentation.Controllers
+{
+    [Route("api/modules")]
+    [ApiController]
+    public class ModuleController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
 
-//        public ModuleController(IServiceManager serviceManager)
-//        {
-//            _serviceManager = serviceManager;
-//        }
+        public ModuleController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
 
-//        [HttpGet("{id}")]
-//        public async Task<ActionResult<ModuleDto>> GetOneModule(int id)
-//        {
-//            var courseDto = await _serviceManager.CourseService.GetCourseByIdAsync(id);
-//            return Ok(courseDto);
-//        }
-//    }
-//}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ModuleDto>> GetOneModule(int id)
+        {
+            var moduleDto = await _serviceManager.ModuleService.GetModuleByIdAsync(id);
+            return Ok(moduleDto);
+        }
+    }
+}
