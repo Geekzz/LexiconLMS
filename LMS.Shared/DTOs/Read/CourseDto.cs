228d577a14f8dda075af1dc9007267f0516ccaa9
Deddel <83078233+deddel@users.noreply.github.com>
2025-01-17 13:11:33 +0100

    Add dummy link to files in components in the Course Overview

--- LMS.Shared/DTOs/Read/CourseDto.cs
+++ LMS.Shared/DTOs/Read/CourseDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,19 @@ namespace LMS.Shared.DTOs.Read
     {
         // ToDo test with init on all reads
         public int CourseId { get; set; }
+        [Display(Name = "Kursnamn")]
         public string Name { get; set; }
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
+        [Display(Name = "Startdatum")]
         public DateTime StartDate { get; set; }
+        [Display(Name = "Slutdatum")]
         public DateTime EndDate { get; set; }
-
+        [Display(Name = "Kursdokument")]
         public List<CourseDocumentDto> CourseDocuments { get; set; }
+        [Display(Name = "Moduler")]
         public List<ModuleDto> Modules { get; set; }
-
-        //public List<ActivityDto> Activities { get; set; }
-
+        [Display(Name = "Kursdeltagare")]
         public List<UserDto> Users { get; set; }
     }
 }
