0af817980ab054353d4224a7b00a26670b697647
Alexander Gustafsson <alegus753@gmail.com>
2025-01-17 15:22:51 +0100

    connected roles to user

--- LMS.Infrastructure/Data/SeedData.cs
+++ LMS.Infrastructure/Data/SeedData.cs
@@ -28,7 +28,8 @@ public static class SeedData
 
             try
             {
-                await CreateRolesAsync([adminRole]);
+                var roleNames = new[] { adminRole, studentRole };
+                await CreateRolesAsync(roleNames);
                 await GenerateUsersAsync(5);
                 await db.SaveChangesAsync();
             }
@@ -72,7 +73,29 @@ public static class SeedData
         foreach (var user in users)
         {
             var result = await userManager.CreateAsync(user, passWord);
+        }
+
+        for (var i = 0; i < users.Count; i++)
+        {
+            IdentityResult result;
+            if (i == 0)
+            {
+                result = await userManager.AddToRoleAsync(users[i], adminRole);
+            }
+            else
+            {
+                result = await userManager.AddToRoleAsync(users[i], studentRole);
+            }
             if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
+        }
+    }
+
+    private static async Task AddUserToRoleAsync(ApplicationUser user, string roleName)
+    {
+        if (!await userManager.IsInRoleAsync(user, roleName))
+        {
+            var result = await userManager.AddToRoleAsync(user, roleName);
+            if (!result.Succeeded) throw new Exception(string.Join(", ", result.Errors.Select(x => "Code " + x.Code + " Description" + x.Description)));
         }
     }
 }
