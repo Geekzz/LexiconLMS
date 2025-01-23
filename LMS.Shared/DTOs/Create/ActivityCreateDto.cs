956b75ebb46a8de6accaba7e5ccb5daf3ff283b2
Geekzz <jjohan91@hotmail.com>
2025-01-23 22:32:32 +0100

    Added correct validation for edit course

--- LMS.Shared/DTOs/Create/ActivityCreateDto.cs
+++ LMS.Shared/DTOs/Create/ActivityCreateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,14 @@ namespace LMS.Shared.DTOs.Create
     public class ActivityCreateDto
     {
         public int ModuleId { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Required]
         public DateTime StartDate { get; set; }
+        [Required]
         public DateTime EndDate { get; set; }
-
+        [Required]
         public int ActivityTypeId { get; set; }
     }
 }
